4cdb8f1a7148260c6e785c2521569265ac4a7467
Eunryong <srdn45@gmail.com>
2025-08-04 21:53:30 +0900

    fix: docker compose file

--- geminitest.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyDaunfDoBRIoY1r52WTRxpX9lN3Blfj1RU")
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash-lite-preview-06-17", contents="Explain how AI works in a few words"
-)
-print(response)
